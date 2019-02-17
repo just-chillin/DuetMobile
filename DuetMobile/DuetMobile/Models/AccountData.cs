04e2ddd68370100b6403160f199abeaa064f4377
Colin Sivak <crs4665@rit.edu>
2019-02-17 07:31:16 -0500

    it dun

--- DuetMobile/DuetMobile/Models/AccountData.cs
+++ DuetMobile/DuetMobile/Models/AccountData.cs
@@ -1,26 +1,40 @@
 ﻿using System;
+using Xamarin.Essentials;
 using Newtonsoft.Json;
 namespace DuetMobile.Models
 {
     public class AccountData
     {
-        public string ApiKey;
         public Survey Survey;
+        public string YearOfBirth;
+        public string Gender;
+        public string FirstName;
+        public string LastName;
         public static AccountData AccountDataThatExistsAllTheTime = new AccountData();
         private AccountData() { }
-        public AccountData RetreiveLocalData()
+
+        const string LOCAL_STORAGE_KEY = "AccountData";
+
+        public static AccountData RetreiveLocalData()
         {
-            throw new NotImplementedException();
+            var json = Preferences.Get(LOCAL_STORAGE_KEY, null);
+            return JsonConvert.DeserializeObject<AccountData>(json);
         }
 
-        public void UpdateServer()
+        public static bool Exists()
         {
+            return RetreiveLocalData() != null;
+        }
 
+        public void SignUp()
+        {
+            var jsonAccountData = JsonSeralize();
         }
 
         public void SaveLocally()
         {
-
+            var json = JsonSeralize();
+            Preferences.Set(LOCAL_STORAGE_KEY, json);
         }
 
         public string JsonSeralize()
