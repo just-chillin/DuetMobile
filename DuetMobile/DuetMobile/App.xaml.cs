9d6909df66165171066d81ad78de536eb2f4dc3c
Joey Nepravishta <tjn9818@ad.rit.edu>
2019-02-16 19:40:42 -0500

    can go to new page when creating an account

--- DuetMobile/DuetMobile/App.xaml.cs
+++ DuetMobile/DuetMobile/App.xaml.cs
@@ -14,7 +14,7 @@ namespace DuetMobile
             InitializeComponent();
 
 
-            MainPage = new DuetMobile.loginSignUp(); // set the login to the mainpage
+            MainPage = new NavigationPage ( new DuetMobile.loginSignUp()); // set the login to the mainpage
         }
 
         protected override void OnStart()
