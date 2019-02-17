2f754d996b725128c0eaec134386b1232a8dcd26
Colin Sivak <crs4665@rit.edu>
2019-02-17 07:09:29 -0500

    stubbed it out

--- DuetMobile/DuetMobile/Models/AccountData.cs
+++ DuetMobile/DuetMobile/Models/AccountData.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using Newtonsoft.Json;
 namespace DuetMobile.Models
 {
     public class AccountData
     {
-        public AccountData()
+        public string ApiKey;
+        public Survey Survey;
+        public static AccountData AccountDataThatExistsAllTheTime = new AccountData();
+        private AccountData() { }
+        public AccountData RetreiveLocalData()
         {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateServer()
+        {
+
+        }
+
+        public void SaveLocally()
+        {
+
+        }
+
+        public string JsonSeralize()
+        {
+            return JsonConvert.SerializeObject(this);
         }
     }
 }
