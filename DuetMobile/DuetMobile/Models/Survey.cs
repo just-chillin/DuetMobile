b800517f2a1e204890cbb04af58ea5d77f68026c
Colin Sivak <crs4665@rit.edu>
2019-02-17 06:55:31 -0500

    * DuetMobile.csproj: added some shit
    * Survey.cs: survey done
    
    * UserData.cs: not done

--- DuetMobile/DuetMobile/Models/Survey.cs
+++ DuetMobile/DuetMobile/Models/Survey.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using Xamarin.Essentials;
+using Newtonsoft.Json;
 namespace DuetMobile.Models
 {
     public class Survey
     {
-        public Survey()
+        public String music;
+        public String MoviesAndTV;
+        public String Games;
+        public String Books;
+
+        public string JsonSeralize()
         {
+            return JsonConvert.SerializeObject(this);
         }
     }
 }
