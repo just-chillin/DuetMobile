e59bd713d1182e74d4996e0a1c486ef5b30210fb
joeyn <tjn9818@rit.edu>
2019-02-17 03:19:50 -0500

    added a bunch of stuff

--- DuetMobile/DuetMobile/Survey.xaml.cs
+++ DuetMobile/DuetMobile/Survey.xaml.cs
@@ -19,8 +19,9 @@ namespace DuetMobile
 
         private void usersProfile(object sender, EventArgs e)
         {
-            //string music = 
-           // string movies = movies.Text;
+            
+            //string music = musi
+            //string movies = movies.Text;
            // DisplayAlert("Values entered:", name + " " + pass, "ok");
             Navigation.PushAsync(new usersProfile());
         }
